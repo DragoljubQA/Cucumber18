df798c18d67cf3cbc00e9503e0e69e418f997ec6
drago <dragoljub.qa@gmail.com>
2024-01-29 21:30:32 +0100

    Initial commit

--- src/test/java/Pages/ProfilePage.java
+++ src/test/java/Pages/ProfilePage.java
@@ -1,2 +1,17 @@
-package Pages;public class ProfilePage {
+package Pages;
+
+import Steps.StepDefinition;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProfilePage extends StepDefinition {
+
+    public ProfilePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "shopping_cart_container")
+    public WebElement cartButton;
+
 }
